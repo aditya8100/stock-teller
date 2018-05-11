f4add014368fd6721837db31d0534453bd4ede3b
Aditya Deepak <deepak2@purdue.edu>
2018-05-11 12:59:59 +0400

    Corrected website in index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,3 @@
-$.get('https://www.freecodecamp.com/' + name, function(response) {
+$.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies' + name, function(response) {
   console.log(response);
 });
