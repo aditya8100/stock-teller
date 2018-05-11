f3038dcc088676877fbb2aaceb59521459b3891f
Aditya Deepak <deepak2@purdue.edu>
2018-05-11 13:20:01 +0400

    Try different

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-let url = "http://anyorigin.com/go?url=" + encodeURIComponent("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies") + "&callback=?";
+let url = "http://www.whateverorigin.org/get?url=" + encodeURIComponent("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies") + "&callback=?";
 $.get(url, function(response) {
   console.log(response);
 });
