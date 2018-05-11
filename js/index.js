9e66ae8cf16782928235b22ae92ea411428867b0
Aditya Deepak <deepak2@purdue.edu>
2018-05-11 15:22:53 +0400

    Error correction

--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@ s_p500.forEach(function(stock) {
       console.log(response);
       var dataString = JSON.stringify(response);
       let data = JSON.parse(dataString);
-      console.log(data["Time Series (Daily)"][formatDate(new Date())]);
+      console.log(data["Time Series (Daily)"]["2018-05-10"]);
     });
     var start = new Date().getTime();
     for (var i = 0; i < 1e7; i++) {
