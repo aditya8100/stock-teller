92a5a0f0db360be90213f66723313b1f7d34e48c
Aditya Deepak <deepak2@purdue.edu>
2018-05-11 13:24:31 +0400

    added access header

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-let url = "http://www.whateverorigin.org/get?url=" + encodeURIComponent("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies") + "&callback=?";
+let url = "https://cors-anywhere.herokuapp.com/" + "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies";
 $.get(url, function(response) {
   console.log(response);
 });
