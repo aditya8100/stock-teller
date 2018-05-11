18ca026dc5cb782a01ceba547fb1c3ec0d96ab03
Aditya Deepak <deepak2@purdue.edu>
2018-05-11 13:13:53 +0400

    Fixed No-Access Error

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
-$.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies', function(response) {
+$.get('http://anyorigin.com/go?url=" + encodeURIComponent("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies") + name + "&callback=?', function(response) {
   console.log(response);
 });
+
