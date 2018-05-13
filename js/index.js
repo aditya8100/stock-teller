005c5a3abfd3a79a3a9e0448efd507fb93911bc7
Aditya Deepak <deepak2@purdue.edu>
2018-05-13 11:48:35 +0400

    Added company names

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $('#content').append("<img class=\"loading\" src=\"assets/loading.gif\" height=\"160\" width=\"241\" margin=\"0\" padding=\"0\"/>")
-
+var losers, percentages;
 // Initial
 $.get("https://cors-anywhere.herokuapp.com/https://stock-predictor-server.herokuapp.com/losers", function(response) {
   let responseAsString = JSON.stringify(response);
@@ -8,8 +8,8 @@ $.get("https://cors-anywhere.herokuapp.com/https://stock-predictor-server.heroku
   console.log(responseAsJSON.losers)
   console.log(responseAsJSON.percentages)
 
-  var losers = responseAsJSON.losers
-  var percentages = responseAsJSON.percentages
+  losers = responseAsJSON.losers
+  percentages = responseAsJSON.percentages
 
   for(var i=0; i < percentages.length - 1; i+=1) {
     for(var j=0; j < percentages.length - 1 - i; j+=1) {
@@ -27,7 +27,7 @@ $.get("https://cors-anywhere.herokuapp.com/https://stock-predictor-server.heroku
 
 
   for(var i=0; i < percentages.length; i++) {
-    percentages[i] = percentages[i].toFixed(2);
+    percentages[i] = (percentages[i] * 1).toFixed(2);
   }
   
   console.log(losers)
@@ -36,10 +36,19 @@ $.get("https://cors-anywhere.herokuapp.com/https://stock-predictor-server.heroku
   losers.forEach(element => {
     console.log('Inside foreach initial')
     $('.loading').remove();
-    let toAppend = "<p class=\"loser\">" + element + "&nbsp;&nbsp;&nbsp;&nbsp;🔻" + percentages[i] + "%</p>";
-    $('.content').append(toAppend);
+    $.get("https://cors-anywhere.herokuapp.com/https://www.investopedia.com/markets/stocks/" + element.toLowerCase() + "/historical/", function(response) {
+        const toCheck = "<span style=\"text-transform:none;\" id=\"quoteName\">";
+        var indexOfName = response.indexOf(toCheck) + toCheck.length;
+        var indexOfNameEnd = response.indexOf("</span>", indexOfName + 1);
+        var name = response.substring(indexOfName,indexOfNameEnd);
+        name = name.trim();
+        console.log("name: " + name)
+        let toAppend = "<p class=\"loser\">" + name + " - " + element + "&nbsp;&nbsp;&nbsp;&nbsp;🔻" + percentages[i] + "%</p>";
+        $('.content').append(toAppend);
+        i = i + 1;
+    });
     // $('#content').append("<img class=\"loading\" src=\"assets/loading.gif\" height=\"100\" width=\"100\" margin=\"0\" padding=\"0\"/>")
-    i = i + 1;
+    
   });
 });
 
@@ -56,8 +65,8 @@ setInterval(function() {
     console.log(responseAsJSON.losers)
     console.log(responseAsJSON.percentages)
 
-    var losers = responseAsJSON.losers
-    var percentages = responseAsJSON.percentages
+    var losersRefresh = responseAsJSON.losers
+    var percentagesRefresh = responseAsJSON.percentages
     for(var i=0; i < percentages.length - 1; i+=1) {
       for(var j=0; j < percentages.length - 1 - i; j+=1) {
         if (percentages[j] < percentages[j+1]) {
@@ -73,8 +82,25 @@ setInterval(function() {
     }
 
     for(var i=0; i < percentages.length; i++) {
-      percentages[i] = percentages[i].toFixed(2);
+      percentages[i] = (percentages[i] * 1).toFixed(2);
     }
+
+    if (losers.length == losersRefresh.length) {
+      var checker = 0;
+      for(var i=0; i < losers.length; i++) {
+        if (losers[i] != losersRefresh[i]) {
+          checker = 1;
+        }
+      }
+
+      if (checker == 0) {
+        console.log('Inside return')
+        return;
+      }
+    }
+
+    losers = losersRefresh;
+    percentages = percentagesRefresh
 
     console.log(losers)
     console.log(percentages)
@@ -83,12 +109,21 @@ setInterval(function() {
     losers.forEach(element => {
       console.log('Inside foreach refresh')
       // $('.loading').remove();
-      let toAppend = "<p class=\"loser\">" + element + "&nbsp;&nbsp;&nbsp;&nbsp;🔻" + percentages[i] + "%</p>";
-      $('.content').append(toAppend);
+      $.get("https://cors-anywhere.herokuapp.com/https://www.investopedia.com/markets/stocks/" + element.toLowerCase() + "/historical/", function(response) {
+        const toCheck = "<span style=\"text-transform:none;\" id=\"quoteName\">";
+        var indexOfName = response.indexOf(toCheck) + toCheck.length;
+        var indexOfNameEnd = response.indexOf("</span>", indexOfName + 1);
+        var name = response.substring(indexOfName,indexOfNameEnd);
+        name = name.trim();
+        console.log("name: " + name)
+        let toAppend = "<p class=\"loser\">" + name + " - " + element + "&nbsp;&nbsp;&nbsp;&nbsp;🔻" + percentages[i] + "%</p>";
+        $('.content').append(toAppend);
+        i = i + 1;
+      });
       // $('#content').append("<img class=\"loading\" src=\"assets/loading.gif\" height=\"100\" width=\"100\" margin=\"0\" padding=\"0\"/>")
-      i = i + 1;
+     
     });
   });
 
   // $('.loading').remove();
-}, 1000);
+}, 600000);
