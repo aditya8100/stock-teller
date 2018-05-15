70e23f9cf1c33b5d6237f33077cf63b2ee60699f
Aditya Deepak <30564924+aditya8100@users.noreply.github.com>
2018-05-15 22:19:48 +0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ $.get("https://cors-anywhere.herokuapp.com/https://stock-predictor-server.heroku
 
   losers = responseAsJSON.losers
   percentages = responseAsJSON.percentages
-  // date = responseAsJSON.date
+  date = responseAsJSON.date
   date = "(" + date + ")";
   if (date == "()") {
     date = "";
